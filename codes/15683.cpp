24a0374b89c988f40d89e37ac7faf2ce4fed9213
bmryu0501 <bmryu0501@gmail.com>
2022-09-01 12:27:09 +0900

    15683 on work

--- codes/15683.cpp
+++ codes/15683.cpp
@@ -9,8 +9,8 @@ using namespace std;
 #define MAX_CAMERA 8
 
 /* custom */
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
 
 /* variables */
 int N, M;
@@ -23,43 +23,158 @@ int min(int a, int b) {
     return a<b?a:b;
 }
 
-int fill_area(int n, int m, int camera_num, vector<vector<int>> v) {
+int count_zero(vector<vector<int>> v) {
+    int cnt = 0;
+    for(int i=0;i<N;i++)
+        for(int j=0;j<M;j++)
+            if(v[i][j]==0)
+                cnt++;
+
+    return cnt;
+}
+
+int fill_area(int camera_num, vector<vector<int>> v) {
+    cout << '[' << camera_num << ']' << '\n'; 
+
     int x_origin = arr_camera[camera_num].first;
     int y_origin = arr_camera[camera_num].second;
+
+    int min_blind_spot = MAX_N * MAX_M;
+
     if(camera_num > num_camera) {
-        int cnt = 0;
-
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<M;j++){
-                if(v[i][j] == 0) cnt++;
-            }
-
-            return cnt;
-        }
+        return count_zero(v);
     }
 
-    switch (v[x_origin][y_origin]) {
-        int x_curr = x_origin;
-        int y_curr = y_origin;
+    int x = x_origin;
+    int y = y_origin;
 
+    switch (v[x_origin][y_origin]) {
         case 1:
             for(int i=0;i<4;i++) {
                 vector<vector<int>> v_tmp;
                 copy(v.begin(), v.end(), v_tmp.begin());
-                while(x_curr >= 0 && x_curr < N && y_curr >= 0 && y_curr < M && v[x_curr][y_curr] != 6) {
-                    x_curr+=dx[i];
-                    y_curr+=dy[i];
 
-                    if(v[x_curr][y_curr] == 0) {
-                        
-                    }
-                        
+                while(x >= 0 && x < N && y >= 0 && y < M) {
+                    x+=dx[i];
+                    y+=dy[i];
+
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                int blind_spot = fill_area(camera_num+1, v_tmp);
+                if(blind_spot < min_blind_spot) min_blind_spot = blind_spot;
+            }
+            return min_blind_spot;
+        case 2:
+            for(int i=0;i<2;i++) {
+                vector<vector<int>> v_tmp;
+                copy(v.begin(), v.end(), v_tmp.begin());
+
+                while(x >= 0 && x < N && y >= 0 && y < M && v[x][y] != 6) {
+                    x+=dx[i];
+                    y+=dx[i];
+                    
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                x = x_origin;
+                y = y_origin;
+                while(x >= 0 && x < N && y >= 0 && y < M && v[x][y] != 6) {
+                    x+=dx[i+2];
+                    y+=dx[i+2];
+                    
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                int blind_spot = fill_area(camera_num+1, v_tmp);
+                if (blind_spot < min_blind_spot) min_blind_spot = blind_spot;
+            }
+            return min_blind_spot;
+        case 3:
+            for(int i=0;i<4;i++) {
+                vector<vector<int>> v_tmp;
+                copy(v.begin(), v.end(), v_tmp.begin());
+
+                while(x >= 0 && x < N && y >= 0 && y < M) {
+                    x+=dx[i];
+                    y+=dy[i];
+
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                x = x_origin;
+                y = y_origin;
+                while(x >= 0 && x < N && y >= 0 && y < M && v[x][y] != 6) {
+                    x+=dx[(i+1)%4];
+                    y+=dx[(i+1)%4];
+                    
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                int blind_spot = fill_area(camera_num+1, v_tmp);
+                if(blind_spot < min_blind_spot) min_blind_spot = blind_spot;
+            }
+            return min_blind_spot;
+        case 4:
+            for(int i=0;i<4;i++) {
+                vector<vector<int>> v_tmp;
+                copy(v.begin(), v.end(), v_tmp.begin());
+
+                while(x >= 0 && x < N && y >= 0 && y < M) {
+                    x+=dx[i];
+                    y+=dy[i];
+
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                x = x_origin;
+                y = y_origin;
+                while(x >= 0 && x < N && y >= 0 && y < M && v[x][y] != 6) {
+                    x+=dx[(i+1)%4];
+                    y+=dx[(i+1)%4];
+                    
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                x = x_origin;
+                y = y_origin;
+                while(x >= 0 && x < N && y >= 0 && y < M && v[x][y] != 6) {
+                    x+=dx[(i+2)%4];
+                    y+=dx[(i+2)%4];
+                    
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
+                }
+
+                int blind_spot = fill_area(camera_num+1, v_tmp);
+                if(blind_spot < min_blind_spot) min_blind_spot = blind_spot;
+            }
+            return min_blind_spot;
+        case 5:
+            vector<vector<int>> v_tmp;
+            copy(v.begin(), v.end(), v_tmp.begin());
+
+            for(int i=0;i<4;i++) {
+                while(x >= 0 && x < N && y >= 0 && y < M) {
+                    x+=dx[i];
+                    y+=dy[i];
+
+                    if(v[x][y] == 6) break;
+                    if(v[x][y] == 0) v[x][y] = -1;
                 }
             }
-        case 2:
-        case 3:
-        case 4:
-        case 5:
+
+            int blind_spot = fill_area(camera_num+1, v_tmp);
+            if(blind_spot < min_blind_spot) min_blind_spot = blind_spot;
+
+            return min_blind_spot;
     }
 }
 
@@ -70,6 +185,8 @@ int main() {
     /* input */
     cin >> N >> M;
     v.resize(N, vector<int>(M, 0));
+
+    cout << v.size() << '\n';
 
     /* solving */
     for(int i=0;i<N;i++) {
@@ -82,6 +199,18 @@ int main() {
         }
     }
 
+    system("PAUSE");
+
+    for(int i=0;i<N;i++) {
+        for(int j=0;j<M;j++) {
+            cout << v[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    system("PAUSE");
 
     /* output */
+    cout << fill_area(0, v) << '\n';
+
 }
