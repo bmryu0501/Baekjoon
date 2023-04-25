c423fcc16858611dae636161078781f5a5eaee7f
bmryu0501 <bmryu0501@gmail.com>
2023-04-25 16:44:45 +0900

    solved 2110

--- solved/Gold/2110.cpp
+++ solved/Gold/2110.cpp
@@ -1,3 +1,8 @@
+/*
+2110번 공유기 설치
+https://www.acmicpc.net/source/49824856
+*/
+
 #include <iostream>
 #include <algorithm>
 
@@ -6,11 +11,11 @@ using namespace std;
 const int MAXN = 2e6;
 int N, C, arr[MAXN];
 
-bool f(int x) {
+bool check(int x) {
     int tmp = 0;
     int cnt = C - 1;
     for(int i = 0; i < N; i++) {
-        if((arr[i] - arr[tmp]) >= x) {
+        if((arr[i] - arr[tmp]) > x) {
             cnt--;
             tmp = i;
         }
@@ -23,10 +28,10 @@ int main() {
     for(int i = 0; i < N; i++) scanf("%d", &arr[i]);
     sort(arr, arr+N);
     int lo = 0, hi = 1e9;
-    while(lo + 1 != hi) {
+    while(lo != hi) {
         int mid = (lo + hi) / 2;
-        if(f(mid)) hi = mid;
-        else lo = mid;
+        if(check(mid)) hi = mid;
+        else lo = mid+1;
     }
 
     printf("%d\n", lo);
