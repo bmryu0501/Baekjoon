9ff859a941b730d4eaa926fd02b41d4cebbbdedb
bmryu0501 <bmryu0501@gmail.com>
2023-06-08 23:38:26 +0900

    solving 18808

--- unsolved/18808.cpp
+++ unsolved/18808.cpp
@@ -1,3 +1,10 @@
+/*
+백준 18808번 스티커 붙이기
+https://www.acmicpc.net/problem/18808
+*/
+
+#define DEBUG
+
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
 #include <iostream>
@@ -40,10 +47,27 @@ void rotate(int idx){
     now->C = temp.C;
 }
 
+bool isAvailable(int r, int c, Block* now) {
+    for(int i=0;i<now->R;i++)for(int j=0;j<now->C;j++) {
+        if(r+now->R >= N || c+now->C >= N) continue; // 스티커를 온전히 못붙인다
+        if(notebook[r+i][c+j] && now->shape[i][j]) // 둘 다 1 (스티커를 붙일 수 없다)
+            continue;
+        return true;
+    }
+    return false;
+}
+
 bool check(int idx) {
     Block* now = &blocks[idx];
 
-    //TODO
+    for(int i=0;i<N;i++)for(int j=0;j<M;j++) {
+        if(notebook[i][j]) continue; // 이미 스티커 붙은 곳
+        if(isAvailable(i, j, now)) { // (i, j)에 붙일 수 있으면
+            // 붙인다
+            for(int r=0;r<now->R;r++)for(int c=0;c<now->C;c++) notebook[i+r][j+c] = now->shape[r][c];
+            return true;
+        }
+    }
 }
 
 int main() {
@@ -63,14 +87,33 @@ int main() {
 
     // solve
     for(int k=0;k<K;k++) {
-        for(int d=0;d<4;d++) {
-            if(check(k)) {
+        for(int d=0;d<4;d++) { // 4 방향에 대하여
+            if(check(k)) { // 붙일 수 있으면
                 ans += blocks[k].num;
-                break;
+                break; // 끝냄
             }
+            // 못붙이면 돌린다
             rotate(k);
         }
     }
+
+    #ifdef DEBUG
+    cout << "\n";
+    for(int k=0;k<K;k++) {
+        Block* now = &blocks[k];
+        for(int i=0;i<now->R;i++) {
+            for(int j=0;j<now->C;j++) cout << now->shape[i][j] << ' ';
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++) cout << notebook[i][j] << " ";
+        cout << "\n";
+    }
+    #endif
 
     cout << ans;
 
