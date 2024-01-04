5d0e2d15d20329f9eef05ab049263e79907ce858
Byeongmin <bmryu0501>
2024-01-04 18:41:59 +0900

    solving 1062

--- unsolved/1062.cpp
+++ unsolved/1062.cpp
@@ -11,69 +11,70 @@ char inputs[MAX_N][MAX_LEN]; // input strings
 int bits[MAX_N]; // used bits for each inputs
 int used_char; // used characters in bits
 int acint; // bits for "anta tica"
-int min_unreadable; // answer = N - min_impossible
+int answer;
 
-int min(int a, int b) {
-    return a<b?a:b;
+int max(int a, int b) {
+    return a>b?a:b;
 }
 
 void init() {
     acint = 0;
-    acint | 1 << 'a' - 'a';
-    acint | 1 << 'c' - 'a';
-    acint | 1 << 'i' - 'a';
-    acint | 1 << 'n' - 'a';
-    acint | 1 << 't' - 'a';
+    acint |= (1 << ('a' - 'a'));
+    acint |= (1 << ('c' - 'a'));
+    acint |= (1 << ('i' - 'a'));
+    acint |= (1 << ('n' - 'a'));
+    acint |= (1 << ('t' - 'a'));
+
+    answer = 0;
 
     used_char = acint;
-
-    min_unreadable = N;
 }
 
-void dfs(int using_char, int num_chars, int idx_next) {
-    if(num_chars == K) { // base case
-        int cnt = 0;
+void dfs(int using_char, int num_using, int idx_next) {
+    if(num_using >= K || used_char == using_char) {
+        int readable = 0;
         for(int i=0;i<N;i++) {
-            if(bits[i] & (bits[i] ^ using_char) > 0) cnt++; // check if unreadable
-            if(cnt >= min_unreadable) break; // backtracking
+            if(bits[i] & using_char == bits[i]) readable++;
         }
-        min_unreadable = min(min_unreadable, cnt);
+
+        answer = max(answer, readable);
+
+        return;
     }
 
-    // recursive
-    for(int i=idx_next; i<MAX_K; i++) {
-        if(used_char & (1 << idx_next)) { // if char is used
-            dfs(using_char | (1 << idx_next), num_chars + 1, i+1);
-        }
+    for(int i=idx_next;i<MAX_K;i++) {
+        if((used_char & (1 << i)) > 0) dfs(using_char | (1 << i), num_using+1, i+1);
     }
 }
 
-int check(int N, int K) {
-    if(K < 5) return 0; //acint
-
+void input(int N, int K) {
     init();
 
     for(int i=0;i<N;i++) {
         cin >> inputs[i];
-        bits[i] | acint;
+        bits[i] |= acint;
         int len = strlen(inputs[i]) - 4;
         for(int j=4;j<len;j++) { // from after "anta" to before "tica"
-            bits[i] | 1 << (inputs[i][j] - 'a');
-            used_char | 1 << (inputs[i][j] - 'a');
+            bits[i] |= (1 << (inputs[i][j] - 'a'));
+            used_char |= (1 << (inputs[i][j] - 'a'));
         }
     }
-
-    dfs(acint, 5, 0);
-
-    return N - min_unreadable;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
     cin >> N >> K;
+    input(N, K);
 
-    cout << check(N, K);
+    if(K < 5) { // Cannot even read "anta" and "tica"
+        cout << 0;
+        return 0;
+    }
+
+    dfs(0, 0, 0);
+
+    cout << answer;
 
     return 0;
 }
